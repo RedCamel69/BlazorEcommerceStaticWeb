816c1adde3725cd8f2098f26d1a59386a1d0dd97
Richard Gilbert <RichardJGilbert@hotmail.com>
2022-11-14 16:19:47 +0000

    dummy additional function app poc

---
+++ Shared/Address.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlazorEcommerceStaticWebApp.Shared
+{
+    public class Address
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string Street { get; set; } = string.Empty;
+        public string City { get; set; } = string.Empty;
+        public string State { get; set; } = string.Empty;
+        public string Zip { get; set; } = string.Empty;
+        public string Country { get; set; } = string.Empty;
+    }
+}
