60d670e0a725f1568f5f969a0d2b6d9e307d134d
Richard Gilbert <RichardJGilbert@hotmail.com>
2022-11-16 17:58:17 +0000

    copy sqlite db to D:home if in Azure env

--- Api/StartUp.cs
+++ Api/StartUp.cs
@@ -2,18 +2,46 @@
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Azure.Functions.Extensions.DependencyInjection;
 using BlazorEcommerceStaticWebApp.Api.Data;
+using System.IO;
+using System;
 
 [assembly: FunctionsStartup(typeof(BlazorEcommerceStaticWebApp.Api.StartUp))]
 namespace BlazorEcommerceStaticWebApp.Api
 {
     public class StartUp : FunctionsStartup
     {
+        const string DevEnvValue = "Development";
+        const string DBPath = "school.db";
+        public const string Azure_DBPath = "D:/home/school.db";
+
+        private static void CopyDb()
+        {
+            File.Copy(DBPath, Azure_DBPath);
+            File.SetAttributes(Azure_DBPath, FileAttributes.Normal);
+        }
         public override void Configure(IFunctionsHostBuilder builder)
         {
-            builder.Services.AddDbContext<ApplicationDbContext>(options =>
+            bool isDevEnv = Environment.GetEnvironmentVariable("AZURE_FUNCTIONS_ENVIRONMENT") == DevEnvValue ? true : false;
+
+            if(!isDevEnv && !File.Exists(Azure_DBPath))
             {
-                options.UseSqlite(Utils.GetSQLiteConnectionString());
-            });
+                CopyDb();
+            }
+
+            if (isDevEnv)
+            {
+                builder.Services.AddDbContext<ApplicationDbContext>(options =>
+                {
+                    options.UseSqlite(Utils.GetSQLiteConnectionString());
+                });
+            }
+            else
+            {
+                builder.Services.AddDbContext<ApplicationDbContext>(options =>
+                {
+                    options.UseSqlite(Azure_DBPath);
+                });
+            }
         }
 
         public override void ConfigureAppConfiguration(IFunctionsConfigurationBuilder builder)
