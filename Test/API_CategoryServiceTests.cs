27fa0ead2f4b250425e627ee1ffdcd06af7ee937
Richard Gilbert <RichardJGilbert@hotmail.com>
2023-01-11 18:11:53 +0000

    additional category basic test

--- Test/API_CategoryServiceTests.cs
+++ Test/API_CategoryServiceTests.cs
@@ -44,5 +44,36 @@ namespace Test
             Assert.Contains("ServiceResponse", categories.GetType().Name);
         }
 
+        [Fact]
+        public void GetCategories_Returns_ServiceResponse_Containing_Expected_Categories()
+        {
+
+            var data = new List<Category>
+            {
+                new Category { Name="Cat 1", Visible = true, Deleted=false},
+                new Category { Name="Cat 2", Visible = true, Deleted=false},
+                new Category { Name="Cat 3", Visible = true, Deleted=false}
+            }.AsQueryable();
+
+            var mockHttpContextAccessor = new Mock<IHttpContextAccessor>();
+
+            var mockSet = new Mock<DbSet<Category>>();
+            mockSet.As<IQueryable<Category>>().Setup(m => m.Provider).Returns(data.Provider);
+            mockSet.As<IQueryable<Category>>().Setup(m => m.Expression).Returns(data.Expression);
+            mockSet.As<IQueryable<Category>>().Setup(m => m.ElementType).Returns(data.ElementType);
+            mockSet.As<IQueryable<Category>>().Setup(m => m.GetEnumerator()).Returns(() => data.GetEnumerator());
+
+            var mockContext = new Mock<ApplicationDbContext>();
+            mockContext.Setup(c => c.Categories).Returns(mockSet.Object);
+
+            var service = new CategoryService(mockContext.Object, mockHttpContextAccessor.Object);
+            var categories = service.GetCategories();
+
+            Assert.Contains("ServiceResponse", categories.GetType().Name);
+            Assert.True(categories.Data.Count==3);
+            Assert.True(categories.Data.FirstOrDefault().Name == "Cat 1");
+
+        }
+
     }
 } 
